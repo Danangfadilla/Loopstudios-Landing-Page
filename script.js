eab7db00c4ff20a21c9442e6a5eecbc2f9998810
Danang Aprias Noor Fadilla <68679153+Danangfadilla@users.noreply.github.com>
2022-05-31 00:08:28 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    var menu = document.getElementById("mobile-links");
+    menu.style.display = "block";
+}
+
+function closeMenu() {
+    var menu = document.getElementById("mobile-links");
+    menu.style.display = "none";
+}
